f0e3222a3430e30167b760b59931977ad7ab6fb8
Muhammad Abdullah Khabir <abdullah@abdullah.solutions>
2023-09-26 08:03:53 +0500

    adding outbound rules

--- main.tf
+++ main.tf
@@ -28,6 +28,12 @@ resource "aws_security_group" "http_ingress" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_instance" "myec2" {
