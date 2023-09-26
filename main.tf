807620efef72492cea87eb01ce23ff69fd0aa745
Muhammad Abdullah Khabir <abdullah@abdullah.solutions>
2023-09-26 07:46:57 +0500

    adding ssh rule

--- main.tf
+++ main.tf
@@ -8,25 +8,31 @@ resource "aws_key_pair" "mysshkey" {
 }
 
 resource "aws_security_group" "http_ingress" {
-    name = "http-ingress"
-    description = "Allow inbound HTTP traffic"
-    ingress {
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-    ingress {
-        from_port = 8080
-        to_port = 8080
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  name        = "http-ingress"
+  description = "Allow inbound HTTP traffic"
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_instance" "myec2" {
-  ami           = var.ami
-  instance_type = var.ec2_type
-  key_name      = aws_key_pair.mysshkey.key_name
+  ami                    = var.ami
+  instance_type          = var.ec2_type
+  key_name               = aws_key_pair.mysshkey.key_name
   vpc_security_group_ids = [aws_security_group.http_ingress.id]
 }
